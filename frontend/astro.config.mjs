ee7148db1d53522dfa631258c83d7161cea0df0a
Samuel Shuert <me@thecoded.prof>
2024-12-24 19:45:54 -0500

    feat: Cookies & colors
    Note: Could only get cookies to work with SSR, need to take a look into
    getting them normally

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -6,5 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
 	site: import.meta.env.SITE_URL,
-	integrations: [tailwind()]
+	integrations: [tailwind()],
+	output: `server`
 });
