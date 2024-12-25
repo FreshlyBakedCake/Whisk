b7fc98be412ea64312c3c710996b883727a964f8
Samuel Shuert <coded@clicks.codes>
2024-12-24 22:04:16 -0500

    feat: Basic box containing post title and pubDate

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,9 +1,15 @@
-import { getEntry } from "astro:content"
-
+import { getCollection, getEntry } from "astro:content"
 
 
 export const formatTitle = async (postId: string): Promise<string> => {
 	let post = await getEntry("blog", postId);
 
 	return post?.data.displayName ?? post?.filePath?.split("/").at(-1)?.replaceAll("%20", " ") ?? ""
+}
+
+export const findPostsWithTag = async (tag: string): Promise<string[]> => {
+	let posts = await getCollection("blog", (post) => {
+		return post.data.tags?.includes(tag)
+	});
+	return posts.map(p => p.id);
 }
