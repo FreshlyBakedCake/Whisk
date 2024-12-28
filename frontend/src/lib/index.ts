8443120d05f1f00ee76b7d796e46ed2cc679beff
Skyler Grey <sky@a.starrysky.fyi>
2024-12-28 12:17:32 +0000

    feat(lib): Improve page title handling
    Previously we had a fairly rudimentary way of getting the page title
    which didn't take into account any url-encoded characters beyond space,
    didn't strip off the markdown file extension and didn't have any way to
    remove the ID prefix.
    
    To remove my Johnny-decimal-style-prefix I am currently using
    PUBLIC_TITLE_REGEX='(M36\.31\+[0-9]+ )?(?<title>.*)' pnpm run dev
    
    Change-Id: I8254e0c7a6d3513366df091999c2517599ea8487

--- frontend/src/lib/index.ts
+++ frontend/src/lib/index.ts
@@ -1,10 +1,37 @@
 import { getCollection, getEntry } from "astro:content"
 
-
-export const formatTitle = async (postId: string): Promise<string> => {
+export const formatTitle = async (postId: string): Promise<string | undefined> => {
 	let post = await getEntry("blog", postId);
 
-	return post?.data.displayName ?? post?.filePath?.split("/").at(-1)?.replaceAll("%20", " ") ?? ""
+	if (!post) {
+		return undefined;
+	}
+
+	if (post.data.displayName) {
+		return post.data.displayName;
+	}
+
+	if (!post.filePath) {
+		return undefined;
+	}
+
+	let name = post.filePath.split("/").at(-1)!;
+	
+	if (name.endsWith(".md")) {
+		name = name.slice(0, name.length - 3);
+	}
+
+	name = decodeURI(name);
+
+	if (import.meta.env.PUBLIC_TITLE_REGEX) {
+		const matches = name.match(import.meta.env.PUBLIC_TITLE_REGEX);
+		
+		if (matches?.groups?.title) {
+			name = matches.groups.title;
+		}
+	}
+	
+	return name;
 }
 
 export const findPostsWithTag = async (tag: string): Promise<string[]> => {
