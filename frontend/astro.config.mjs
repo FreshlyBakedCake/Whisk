0630754657446ea37a763c1f93a2111de7cea483
Skyler Grey <sky@a.starrysky.fyi>
2024-12-28 22:22:18 +0000

    feat: Enable gfm
    GitHub flavored markdown has a lot of common extensions, in particular
    commonmark doesn't have tables, strikethroughs, etc. whereas GitHub
    flavored markdown does. These are used by many platforms that *aren't*
    GitHub, such as Silverbullet or Obsidian and are something that people
    expect to be able to use when writing markdown.
    
    Replaces: I3a5b39c5cb96135e91fb5c8d229b1228d4bfff61
    Change-Id: I1172c032b20fa45330ab5d1c290669a6a9c4d9fe
    Reviewed-on: https://gerrithub.io/c/FreshlyBakedCake/Whisk/+/1206478
    Reviewed-by: Skyler Grey <skyler3665@gmail.com>
    Tested-by: Skyler Grey <skyler3665@gmail.com>

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,10 +2,16 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
+import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
 export default defineConfig({
 	site: import.meta.env.SITE_URL,
 	integrations: [tailwind()],
-	output: `server`
+	output: `server`,
+	markdown: {
+		remarkPlugins: [
+			remarkGfm,
+		]
+	}
 });
