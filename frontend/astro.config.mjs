be0697e4de10ae42ca2f11469d83427d14bcd6a4
Skyler Grey <sky@a.starrysky.fyi>
2024-12-29 23:17:56 +0000

    wip: messing around to try to get images to work

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,8 +2,27 @@
 import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
-import remarkWikiLink from '@portaljs/remark-wiki-link';
+import remarkWikiLink, { getPermalinks } from '@portaljs/remark-wiki-link';
 import remarkGfm from 'remark-gfm';
+import path from 'node:path';
+import deasync from 'deasync';
+
+/** @type {(permalink: string) => Promise<string>} */
+async function permalinkToHref(permalink) {
+	if (permalink.endsWith(".md")) {
+		// TODO: return URL of blog post if it is indeed a blog post
+		return permalink;
+	}
+	
+	const assetPath = path.join(
+		process.env.SPACE_PATH ??
+		process.env.BLOG_PATH ??
+		process.exit(1), permalink);
+
+	console.log({assetPath});
+
+	return await import(path.toString());
+}
 
 // https://astro.build/config
 export default defineConfig({
@@ -12,7 +31,16 @@ export default defineConfig({
 	output: `static`,
 	markdown: {
 		remarkPlugins: [
-			remarkWikiLink,
+			[remarkWikiLink, {
+				pathFormat: "raw",
+				/** @type {(name: string) => string[]} */
+				pageResolver: name => [name],
+				permalinks: getPermalinks(
+					process.env.SPACE_PATH
+					?? process.env.BLOG_PATH
+					?? process.exit(1)),
+				hrefTemplate: permalinkToHref,
+			}],
 			remarkGfm,
 		]
 	}
